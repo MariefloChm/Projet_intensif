2113b746b666947458481b397c559c58fb64a87e
Khadimou <khadimou-rassoul.diop@ens.uvsq.fr>
2023-10-12 23:02:05 +0200

    Interface de l'application Be Linked

---
+++ BeLinked/app1/urls.py
@@ -0,0 +1,12 @@
+from django.urls  import path
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('contact', views.contact_view, name='contact'),
+    path('sign_up/',views.inscription_view, name='sign_up'),
+    path('login/',views.login_view, name='login'),
+    path('change_language/',views.change_language, name='change_language'),
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
